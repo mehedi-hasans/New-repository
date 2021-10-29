d467940edd409e596871daab06200c20a8a738b6
mehedi115 <shihab3676@gmail.com>
2021-10-29 00:54:18 -0400

    html syntax added

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.menu').slicknav();
+
+$('.slider-full').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.testmonial-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
